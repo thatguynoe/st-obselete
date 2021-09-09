97bd56bd98cacd66a062ded8250fc25c3d9d5a7b
thatguynoe <noereyesrivas@gmail.com>
2021-09-09 02:45:20 -0400

    slightly increase font size

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=19:antialias=true:autohint=true";
-static char *font2[] = { "Joy Pixels:pixelsize=17:antialias=true:autohint=true" };
+static char *font = "mono:pixelsize=20:antialias=true:autohint=true";
+static char *font2[] = { "Joy Pixels:pixelsize=18:antialias=true:autohint=true" };
 static int borderpx = 0;
 
 /*
